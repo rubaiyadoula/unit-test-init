72823dd2160d99804ea1c3a8967547601958022b
rubaiyadoula <rubaiyadoula@gmail.com>
2020-03-14 23:45:12 +0600

    remove main class

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import me.rubaiya.Calculator;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.add(1 ,2);
-    }
-}
