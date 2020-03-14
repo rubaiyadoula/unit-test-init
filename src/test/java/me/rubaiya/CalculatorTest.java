11c542917f8e264975999c0d67394576a190f4f6
rubaiyadoula <rubaiya.doula@itconquest.com>
2020-03-15 01:17:20 +0600

    add test methods for failure

--- src/test/java/me/rubaiya/CalculatorTest.java
+++ src/test/java/me/rubaiya/CalculatorTest.java
@@ -6,10 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
-    void addTest() {
-        Calculator calculator = new Calculator();
-        assertEquals(calculator.add(10, 20), 30);
+    void addCorrectTest() {
+        assertEquals(calculator.add(10, 20), 30, "All is well.");
+    }
+
+    @Test
+    void addIncorrectTest() {
+        assertNotEquals(calculator.add(10, 20), 0, "Failure is the pillar of success.");
+    }
+
+    @Test
+    void addTypeIncorrectTest() {
+        assertNotEquals(calculator.add(10, 20), true, "You're just not my type.");
+    }
+
+    @Test
+    void addNotNullTest() {
+        assertNotNull(calculator.add(10, 30), "No place for emptiness.");
     }
 
 }
